7806d16218df17644e77ef5cf1c9d56cd4f074e0
Thomas Nash <thomas.e.nash@googlemail.com>
2025-12-22 18:28:56 +0000

    install helix

--- install_all.sh
+++ install_all.sh
@@ -2,33 +2,71 @@
 
 set -euo pipefail
 
+install_jq() {
+  if is_installed "jq"; then
+    echo "jq is already installed, skipping..."
+    return 0
+  fi
+
+  sudo apt install -y jq
+}
+
 setup_fzf() {
-  assert_command_not_installed "fzf" "fzf"
+  if is_installed "fzf"; then
+    echo "fzf is already installed, skipping..."
+    return 0
+  fi
 
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
 }
 
-# setup_helix() {
-#   local helix_config_dir=${1}
+setup_helix() {
+  if is_installed "hx"; then
+    echo "hx is already installed, skipping..."
+    return 0
+  fi
 
-#   curl https://github.com/helix-editor/helix/releases/tag/25.07.1
-# }
+  local helix_config_dir="${1}"
+
+  install_jq
+
+  local tmpdir
+  tmpdir="$(mktemp -d)"
+
+  local latest_release_tag
+  latest_release_tag="$(curl -s https://api.github.com/repos/helix-editor/helix/releases/latest | jq -r .tag_name)"
+  curl -L "https://github.com/helix-editor/helix/releases/download/${latest_release_tag}/helix-${latest_release_tag}-x86_64-linux.tar.xz" -o "${tmpdir}/helix.tar.xz"
+  mkdir -p "${tmpdir}/helix"
+  tar -xf "${tmpdir}/helix.tar.xz" -C "${tmpdir}/helix" --strip-components=1
+
+  mkdir -p "${HOME}/.local/helix-${latest_release_tag}"
+  mv "${tmpdir}/helix/hx" "${HOME}/.local/helix-${latest_release_tag}/hx"
+  mv "${tmpdir}/helix/runtime" "${HOME}/.local/helix-${latest_release_tag}/runtime"
+  mv "${tmpdir}/helix/contrib" "${HOME}/.local/helix-${latest_release_tag}/contrib"
+  ln -s "${HOME}/.local/helix-${latest_release_tag}/hx" "${HOME}/.local/bin/hx"
+  rm -r "${tmpdir}"
+
+  cp "${helix_config_dir}/config.toml" "${HOME}/.config/helix/config.toml"
+}
 
 setup_zoxide() {
-  assert_command_not_installed "zoxide" "zoxide"
+  if is_installed "zoxide"; then
+    echo "zoxide is already installed, skipping..."
+    return 0
+  fi
 
   curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
   echo 'eval "$(zoxide init bash --cmd cd)"' >> ~/.bashrc
   echo 'alias cd="z"' >> ~/.bashrc
 }
 
-assert_command_not_installed() {
+is_installed() {
   local cmd="${1}"
-  local command_name="${2}"
-
   if type -P "${cmd}" >/dev/null 2>&1; then
-    err '1' "${command_name} is already installed."
+    return 0
+  else
+    return 1
   fi
 }
 
@@ -53,9 +91,9 @@ install_all() {
   setup_fzf
   setup_zoxide
 
-  # local helix_config_dir="${script_dir}/config/helix"
-  # setup_helix ${helix_config_dir}
-  #
+  local helix_config_dir="${script_dir}/config/helix"
+  setup_helix ${helix_config_dir}
+  
   echo 'Run the command 'source ~/.bashrc''
 }
 
