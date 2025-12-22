78360b766aca164e0099d996b902851c7cdd68f0
Thomas Nash <thomas.e.nash@googlemail.com>
2025-12-22 15:06:02 +0000

    Install zoxide now

--- install_all.sh
+++ install_all.sh
@@ -3,9 +3,7 @@
 set -euf -o pipefail
 
 setup_fzf() {
-  if [ $(command -v fzf | echo ${?}) == 0 ]; then
-    err "1" "fzf is already installed."
-  fi
+  assert_command_already_installed "fzf" "fzf"
 
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
@@ -17,13 +15,35 @@ setup_fzf() {
 #   curl https://github.com/helix-editor/helix/releases/tag/25.07.1
 # }
 
+setup_zoxide() {
+  assert_command_already_installed "zoxide" "zoxide"
+
+  curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+  echo 'eval "$(zoxide init bash --cmd cd)"' >> ~/.bashrc
+  echo 'alias cd="z"' >> ~/.bashrc
+}
+
+assert_command_already_installed() {
+  local cmd="${1}"
+  local command_name="${2}"
+
+  if command -v "${cmd}" >/dev/null 2>&1; then
+    err '1' "${command_name} is already installed."
+  fi
+}
+
 err() {
-  echo "Error: ${2}" >&2
-  exit ${1}
+  local exit_code="${1}"
+  local error_message="${2}"
+
+  echo "Error: ${error_message}" >&2
+  exit "${exit_code}"
 }
 
 log() {
-  echo "Log: ${1}"
+  local log_message="${1}"
+
+  echo "Log: ${log_message}"
 }
 
 install_all() {
@@ -31,9 +51,12 @@ install_all() {
   script_dir="$( cd -- $( dirname -- "${BASH_SOURCE[0]}") &> /dev/null && pwd )"
 
   setup_fzf
+  setup_zoxide
 
   # local helix_config_dir="${script_dir}/config/helix"
   # setup_helix ${helix_config_dir}
+  #
+  echo 'Run the command 'source ~/.bashrc''
 }
 
 install_all ${@}
