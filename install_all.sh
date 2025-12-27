b79c949898ebd253d53755ce4cdbf0858bf4b6b7
Thomas Nash <thomas.e.nash@googlemail.com>
2025-12-27 19:15:50 +0000

    actually add fzf export statement to bashrc

--- install_all.sh
+++ install_all.sh
@@ -20,7 +20,7 @@ setup_fzf() {
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
   echo 'eval "$(fzf --bash)"' >> ~/.bashrc
-  echo 'export PATH="${HOME}/.fzf/bin:${PATH}"'
+  echo 'export PATH="${HOME}/.fzf/bin:${PATH}"' >> ~/.bashrc
 }
 
 setup_helix() {
